053402b06a529bfcb066f70520a491929903f599
Georgi Birdanov <gogobiserov@gmail.com>
2023-12-13 13:01:12 +0200

    implement methods in the ticket and user service class and create a authcontroller

--- bussystemapp/src/main/java/com/example/bussystemapp/service/TicketServiceImplementation.java
+++ bussystemapp/src/main/java/com/example/bussystemapp/service/TicketServiceImplementation.java
@@ -6,6 +6,8 @@ import com.example.bussystemapp.model.User;
 import com.example.bussystemapp.repository.TicketRepository;
 import com.example.bussystemapp.repository.TripRepository;
 import com.example.bussystemapp.repository.UserRepository;
+import com.example.bussystemapp.utils.Status;
+import jakarta.persistence.EntityExistsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -24,15 +26,33 @@ public class TicketServiceImplementation implements TicketService {
     //TO DO
     @Override
     public Ticket createTicket(Ticket ticket) {
-        return null;
+
+        if (ticket.getAssignedTo() != null) {
+            if (userRepository.findByUsername(ticket.getAssignedTo().getUsername()) == null) {
+                throw new IllegalArgumentException("Invalid username");
+            }
+        }
+
+          Ticket newTicket = new Ticket();
+          newTicket.setId(ticket.getId());
+          newTicket.setTitle(ticket.getTitle());
+          newTicket.setStatus(Status.valueOf("AVAILABLE"));
+          newTicket.setTrip(ticket.getTrip());
+          newTicket.setAssignedTo(ticket.getAssignedTo());
+          return ticketRepository.save(newTicket);
     }
 
     //TO DO
     @Override
     public Ticket updateTicket(Ticket ticket, Long id) {
+        Ticket foundTicket  = ticketRepository.findById(id).orElseThrow(EntityExistsException::new);
+        foundTicket.setTitle(ticket.getTitle());
+        foundTicket.setStatus(ticket.getStatus());
+        foundTicket.setTrip(ticket.getTrip());
+        foundTicket.setAssignedTo(ticket.getAssignedTo());
 
-        //  Ticket found = ticketRepository.
-                return null;
+        return ticketRepository.save(foundTicket);
+
     }
 
     @Override
@@ -49,6 +69,6 @@ public class TicketServiceImplementation implements TicketService {
 
     @Override
     public void deleteById(Long id) {
-          ticketRepository.deleteById(id);
+        ticketRepository.deleteById(id);
     }
 }
